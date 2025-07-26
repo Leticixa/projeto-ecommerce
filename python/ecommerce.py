8ba647779c470784209e93485e29e23f84c353e5
Letícia <leticiavieir29@gmail.com>
2025-07-26 00:59:29 -0300

    Adiciona estrutura inicial do projeto ecommerce

--- python/ecommerce.py
+++ python/ecommerce.py
@@ -1,1 +1,19 @@
-print("ola mundo")
+import sqlite3
+import pandas as pd
+
+# Criar o banco no mesmo local do script
+conexao = sqlite3.connect("ecommerce.db")
+
+# Carregar CSVs
+clientes = pd.read_csv(r"C:\Users\leticia\Downloads\projeto e-commerce\python\clientes.csv")
+produtos = pd.read_csv(r"C:\Users\leticia\Downloads\projeto e-commerce\python\produtos.csv")
+pedidos = pd.read_csv(r"C:\Users\leticia\Downloads\projeto e-commerce\python\pedidos.csv")
+
+
+# Criar tabelas
+clientes.to_sql("clientes", conexao, if_exists="replace", index=False)
+produtos.to_sql("produtos", conexao, if_exists="replace", index=False)
+pedidos.to_sql("pedidos", conexao, if_exists="replace", index=False)
+
+print("Banco criado e dados inseridos!")
+conexao.close()
